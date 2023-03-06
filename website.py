eec993114e7c4a8aac460405880df2ce44f1219c
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-06 11:08:25 +0100

    Update website.py

--- website.py
+++ website.py
@@ -2,17 +2,22 @@ import dash
 from dash import dcc
 from dash import html
 import pandas as pd
+import time
 
 # Load the CSV file containing the ETH price data
-df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], sep = ';')
+df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], sep=';')
 
 # Create a Dash app
 app = dash.Dash(__name__)
 
-# Define the layout of the app
+# Define the layout of the app with the interval component
 app.layout = html.Div(children=[
     html.H1(children='Ethereum Price Over Time'),
-
+    dcc.Interval(
+        id='interval-component',
+        interval=5*60*1000, # in milliseconds
+        n_intervals=0
+    ),
     # Add a graph showing the price of ETH over time
     dcc.Graph(
         id='price-graph',
@@ -28,6 +33,24 @@ app.layout = html.Div(children=[
         }
     )
 ])
+
+# Define the function to update the graph data
+@app.callback(dash.dependencies.Output('price-graph', 'figure'),
+              [dash.dependencies.Input('interval-component', 'n_intervals')])
+def update_graph_data(n):
+    # Load the updated CSV file containing the ETH price data
+    df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], sep=';')
+    # Return the updated figure
+    return {
+        'data': [
+            {'x': df['date'], 'y': df['price'], 'type': 'line', 'name': 'ETH price'},
+        ],
+        'layout': {
+            'title': 'ETH Price over Time',
+            'xaxis': {'title': 'Date'},
+            'yaxis': {'title': 'Price ($)'}
+        }
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
