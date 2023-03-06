46267acac2197e87adafe5c72810eebf01492b7a
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-06 11:18:47 +0100

    Update website.py

--- website.py
+++ website.py
@@ -12,7 +12,7 @@ app = dash.Dash(__name__)
 
 # Define the layout of the app with the interval component
 app.layout = html.Div(children=[
-    html.Img(src='https://logo-marque.com/wp-content/uploads/2020/12/Ethereum-Logo.png', style={'width': '200'}),
+    html.Img(src='https://logo-marque.com/wp-content/uploads/2020/12/Ethereum-Logo.png', style={'width': '200px'}),
     dcc.Interval(
         id='interval-component',
         interval=5*60*1000, # in milliseconds
