2a718de43908cc9f433f6c4b33928c892eacc02b
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-06 09:48:46 +0100

    Update website.py

--- website.py
+++ website.py
@@ -1,31 +1,20 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import pandas as pd
+import plotly.express as px
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
 
-# Load the CSV file containing the ETH price data
-df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'])
-
-# Create a Dash app
 app = dash.Dash(__name__)
 
-# Define the layout of the app
-app.layout = html.Div(children=[
-    html.H1(children='Ethereum Price Over Time'),
+df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], header=None)
 
-    # Add a graph showing the price of ETH over time
+fig = px.line(df, x='date', y='price', title='Ethereum Price')
+
+app.layout = html.Div(children=[
+    html.H1(children='Ethereum Price Graph'),
     dcc.Graph(
-        id='price-graph',
-        figure={
-            'data': [
-                {'x': df['date'], 'y': df['price'], 'type': 'line', 'name': 'ETH price'},
-            ],
-            'layout': {
-                'title': 'ETH Price over Time',
-                'xaxis': {'title': 'Date'},
-                'yaxis': {'title': 'Price ($)'}
-            }
-        }
+        id='eth-price-graph',
+        figure=fig
     )
 ])
 
