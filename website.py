ec1f98ddf3d60d72989b0ffd68110375cd0dfc62
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-06 08:51:05 +0100

    Update website.py

--- website.py
+++ website.py
@@ -5,12 +5,16 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
+# Read the CSV file once and store it in memory as a dataframe
 df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv')
+
+# Define the Dash app
 app = dash.Dash(__name__)
 app.layout = html.Div([
     html.H1('Ethereum Price Dashboard'),
     dcc.Graph(id='graph'),
-    html.Div(id='data-table')
+    html.Div(id='data-table'),
+    dcc.Interval(id='interval-component', interval=60*1000, n_intervals=0)
 ], style={'font-family': 'sans-serif'})
 
 @app.callback(
@@ -18,12 +22,14 @@ app.layout = html.Div([
     Input('interval-component', 'n_intervals')
 )
 def update_graph(n):
-    # Get the latest data from the CSV file
-    df = pd.read_csv('eth_prices.csv')
-
+    # Get the latest data from the dataframe
+    latest_df = df.tail(100) # get the last 100 rows
+    x = latest_df['Timestamp']
+    y = latest_df['Price']
+    
     # Create a line chart with Plotly
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df['Timestamp'], y=df['Price'], mode='lines'))
+    fig.add_trace(go.Scatter(x=x, y=y, mode='lines'))
 
     # Set the chart title and axis labels
     fig.update_layout(title='Ethereum Price', xaxis_title='Timestamp', yaxis_title='Price')
@@ -35,26 +41,18 @@ def update_graph(n):
     Input('interval-component', 'n_intervals')
 )
 def update_table(n):
-    # Get the latest data from the CSV file
-    df = pd.read_csv('eth_prices.csv')
-
+    # Get the latest data from the dataframe
+    latest_df = df.tail(10) # get the last 10 rows
+    
     # Create a HTML table with the latest data
     table = html.Table(
         # Header
-        [html.Tr([html.Th(col) for col in df.columns])] +
+        [html.Tr([html.Th(col) for col in latest_df.columns])] +
         # Body
-        [html.Tr([html.Td(df.iloc[i][col]) for col in df.columns]) for i in range(min(len(df), 10))]
+        [html.Tr([html.Td(latest_df.iloc[i][col]) for col in latest_df.columns]) for i in range(len(latest_df))]
     )
 
     return table
 
-app.layout = html.Div([
-    html.H1('Ethereum Price Dashboard'),
-    dcc.Graph(id='graph'),
-    html.Div(id='data-table'),
-    dcc.Interval(id='interval-component', interval=60*1000, n_intervals=0)
-], style={'font-family': 'sans-serif'})
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
-
