e7df3487345d1f856f11f936827a168f5b4bb7c4
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-07 11:46:42 +0100

    Update website.py

--- website.py
+++ website.py
@@ -33,7 +33,7 @@ app.layout = html.Div(children=[
         id='price-graph',
         figure={
             'data': [
-                {'x': df['date'], 'y': df['price'], 'type': 'line', 'name': 'ETH price'},
+                {'x': df['date'], 'y': df['price'], 'mode': 'lines', 'name': 'ETH price', 'line': {'color': '#627eea'}},
             ],
             'layout': {
                 'title': 'ETH Price over Time',
