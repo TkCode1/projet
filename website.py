2b48cc02656a5812231b25ac871e4b159e2cd319
Tim Kacel <105510397+TkCode1@users.noreply.github.com>
2023-03-20 08:57:53 +0100

    Update website.py

--- website.py
+++ website.py
@@ -2,8 +2,6 @@ import dash
 from dash import dcc
 from dash import html
 import pandas as pd
-import time
-import datetime
 
 # Load the CSV file containing the ETH price data
 df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], sep=';')
@@ -20,8 +18,6 @@ app = dash.Dash(__name__)
 # Define the layout of the app with the interval component
 app.layout = html.Div(children=[
     html.Img(src='https://logo-marque.com/wp-content/uploads/2020/12/Ethereum-Logo.png', style={'width': '200px', 'display': 'block', 'margin': '0 auto'}),
-    #html.H1(children=f'ETH last price: ${current_price}'),
-    #changes
     html.H1(id='current-price', style={'text-align': 'center', 'color': '#627EEA'}),
     dcc.Interval(
         id='interval-component',
@@ -43,7 +39,8 @@ app.layout = html.Div(children=[
         }
     ),
 ])
-#changes
+
+# Define the function to update the current price display
 @app.callback(
     dash.dependencies.Output('current-price', 'children'),
     [dash.dependencies.Input('interval-component', 'n_intervals')]
@@ -58,12 +55,23 @@ def update_current_price(n):
     # Return the updated current price display
     return f'ETH last price: ${current_price}'
 
-
 # Define the function to update the graph data
 @app.callback(dash.dependencies.Output('price-graph', 'figure'),
               [dash.dependencies.Input('interval-component', 'n_intervals')])
-)
-
+def update_graph_data(n):
+    # Load the updated CSV file containing the ETH price data
+    df = pd.read_csv('/home/ubuntu/proj/eth_prices.csv', names=['date', 'price'], sep=';')
+    # Return the updated figure
+    return {
+        'data': [
+            {'x': df['date'], 'y': df['price'], 'type': 'line', 'name': 'ETH price', 'line': {'color': '#627EEA'}},
+        ],
+        'layout': {
+            'title': 'ETH Price over Time',
+            'xaxis': {'title': 'Date'},
+            'yaxis': {'title': 'Price ($)'}
+        }
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
